de860577361b2e4e2fc78d9a3622a4b01c9a2617
agebauer16558 <a.gebauer16558@hs-ansbach.de>
2022-06-28 16:10:49 +0200

    first step to throwing animation

--- PA-Gameproduction/Assets/Vorlesung/10-IK/Scripts/Crouch.cs
+++ PA-Gameproduction/Assets/Vorlesung/10-IK/Scripts/Crouch.cs
@@ -1,22 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ludiq;
+using Bolt;
 
 public class Crouch : MonoBehaviour
 {
+    
     private Animator ThirdPersonAnis;
-
+    private bool isCrouching;
+    
     void Start()
     {
         ThirdPersonAnis = GetComponent<Animator>();
     }
     void Update()
     {
+        CrouchLoop();
+    }
+
+    void CrouchLoop()
+    {
         if (Input.GetButtonDown("Fire1"))
-        {
-            bool currentState = (ThirdPersonAnis.GetBool("isCrouching"));
+        { 
+
+            bool currentState = ThirdPersonAnis.GetBool("isCrouching");
 
             ThirdPersonAnis.SetBool("isCrouching", !currentState);
-        }    
+        }
+
+        if (Input.GetButtonUp("Fire1"))
+        {
+
+            bool currentState = ThirdPersonAnis.GetBool("isCrouching");
+
+            ThirdPersonAnis.SetBool("isCrouching", !currentState);
+        }
     }
 }
