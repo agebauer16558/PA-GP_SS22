0a01f16071a7b1fd03f3f3459b07d94879e36049
agebauer16558 <a.gebauer16558@hs-ansbach.de>
2022-07-25 21:19:53 +0200

    bugfix iteminhand would score

--- PA-Gameproduction/Assets/Vorlesung/10-IK/Scripts/Scores.cs
+++ PA-Gameproduction/Assets/Vorlesung/10-IK/Scripts/Scores.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Bolt;
 
 public class Scores : MonoBehaviour
 {
     public int points;
+    public GameObject player;
+
+    private bool stillInHand;
+    void Update()
+    {
+        stillInHand = (bool)Variables.Object(player).Get("itemInHand");
+    }
     private void OnTriggerEnter(Collider other)
     {
-        if(this.CompareTag("prismItem") && other.gameObject.CompareTag("Item"))
+        if(this.CompareTag("prismItem") && other.gameObject.CompareTag("Item")
+            && !stillInHand)
         {
             TrainingRoomController.Instance.UpdatePrismScore(points);
             Destroy(other.gameObject);
         }
-        if (this.CompareTag("goalItem") && other.gameObject.CompareTag("Item"))
+        if (this.CompareTag("goalItem") && other.gameObject.CompareTag("Item")
+            && !stillInHand)
         {
             TrainingRoomController.Instance.UpdateGoalScore(points);
             Destroy(other.gameObject);
         }
-        if (this.CompareTag("goal2Item") && other.gameObject.CompareTag("Item"))
+        if (this.CompareTag("goal2Item") && other.gameObject.CompareTag("Item")
+            && !stillInHand)
         {
             TrainingRoomController.Instance.UpdateGoal2Score(points);
             Destroy(other.gameObject);
